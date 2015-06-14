d5235793fb9dd74bd300ebea564fd35ea4a3a152
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-06-14 16:44:36 -0500

    commit finished project

---
+++ script.js
@@ -0,0 +1,413 @@
+var places = [
+	{
+		name: "Austin Pizza Garden",
+		mapSrc: "Austin+Pizza+Garden",
+		latitude: 30.235678,
+		longitude: -97.85908,
+		content: '<div id="content" style="width:250px;height:300px;">hi</div>',
+		wikipediaContent: 'wikpediaContent',
+		website: "http://www.austin-pizza-garden.com",
+		yelpID: "austin-pizza-garden-austin-2",
+		yelpSnippet: "",
+		yelpImageSrc: "",
+		yelpLink: "",
+		twitterWidget: '<a class="twitter-timeline" href="https://twitter.com/ATXPizzaGarden" data-widget-id="610186640003219456">Tweets by @ATXPizzaGarden</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?"http":"https";if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>',
+		instagramWidget: 'no Instagram account'
+	},
+	{
+		name: "Reds",
+		mapSrc: "Red's+Indoor+Range",
+		latitude: 30.236278,
+		longitude: -97.856536,
+		content: '<div id="content" style="width:250px;height:300px;">hi</div>',
+		wikipediaContent: 'wikpediaContent',
+		website: "http://www.redsguns.com/",
+		yelpID: "reds-indoor-range-austin",
+		yelpSnippet: "",
+		yelpImageSrc: "",
+		yelpLink: "",
+		twitterWidget: "no Twitter account",
+		instagramWidget: 'no Instagram account'
+	},
+	{
+		name: "Freescale",
+		mapSrc: "Freescale+Semiconductor+Inc",
+		latitude: 30.238327,
+		longitude: -97.866148,
+		content: '<div id="content" style="width:250px;height:300px;">hi</div>',
+		wikipediaContent: 'wikpediaContent',
+		website: "http://www.austin-pizza-garden.com",
+		yelpID: "freescale-austin-4",
+		yelpSnippet: "",
+		yelpImageSrc: "",
+		yelpLink: "",
+		twitterWidget: '<a class="twitter-timeline" href="https://twitter.com/Freescale" data-widget-id="605108887839113216">Tweets by @Freescale</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?"http":"https";if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>',
+		instagramWidget: '<iframe src="http://snapwidget.com/sc/?h=ZnJlZXNjYWxlfGlufDE1MHwzfDN8fG5vfDV8ZmFkZUlufG9uU3RhcnR8eWVzfG5v&ve=310515" title="Instagram Widget" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:400px; height:150px"></iframe>'
+	},
+	{
+		name: "Verona",
+		mapSrc: "Verona+Ristorante+Italino",
+		latitude: 30.235072,
+		longitude: -97.878345,
+		content: "hi",
+		wikipediaContent: 'wikpediaContent',
+		website: "http://www.veronaustin.com/",
+		yelpID: "verona-ristorante-italiano-austin-3",
+		yelpSnippet: "",
+		yelpImageSrc: "",
+		yelpLink: "",
+		twitterWidget: '<a class="twitter-timeline" href="https://twitter.com/VeronaAustin" data-widget-id="605113439950733312">Tweets by @VeronaAustin</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?"http":"https";if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>',
+		instagramWidget: 'no Instagram account'
+	},
+	{
+		name: "Natural Gardener",
+		mapSrc: "The+Natural+Gardener",
+		latitude: 30.2572,
+		longitude: -97.89055,
+		content: "hi",
+		wikipediaContent: 'wikpediaContent',
+		website: "http://www.naturalgardeneraustin.com/",
+		yelpID: "the-natural-gardener-austin",
+		yelpSnippet: "",
+		yelpImageSrc: "",
+		yelpLink: "",
+		twitterWidget: '<a class="twitter-timeline" href="https://twitter.com/TheNGAustin" data-widget-id="605114162574794753">Tweets by @TheNGAustin</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?"http":"https";if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>',
+		instagramWidget: '<!-- SnapWidget --><iframe src="http://snapwidget.com/sc/?u=dGhlbmF0dXJhbGdhcmRlbmVyfGlufDE1MHwzfDF8fHllc3wyfGZhZGVJbnxvblN0YXJ0fG5vfHllcw==&ve=310515" title="Instagram Widget" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:400px; height:150px"></iframe>'
+	},
+	{
+		name: "AMD",
+		mapSrc: "AMD",
+		latitude: 30.251333,
+		longitude: -97.863768,
+		content: "hi",
+		wikipediaContent: 'wikpediaContent',
+		website: "http://www.amd.com/",
+		yelpID: "advanced-micro-devices-austin-2",
+		yelpSnippet: "",
+		yelpImageSrc: "",
+		yelpLink: "",
+		twitterWidget: '<a class="twitter-timeline" href="https://twitter.com/AMD" data-widget-id="605115206897770496">Tweets by @AMD</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?"http":"https";if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>',
+		instagramWidget: '<!-- SnapWidget --><iframe src="http://snapwidget.com/sc/?u=YW1kfGlufDE1MHwzfDF8fHllc3wyfGZhZGVJbnxvblN0YXJ0fG5vfHllcw==&ve=310515" title="Instagram Widget" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:400px; height:150px"></iframe>'
+	},
+	{
+		name: "Hecho en Mexico",
+		mapSrc: "Hecho+En+Mexico",
+		latitude: 30.228475,
+		longitude: -97.863356,
+		content: "hi",
+		wikipediaContent: 'wikpediaContent',
+		website: "http://hechoenmexico-restaurant.com/",
+		yelpID: "hecho-en-mexico-austin",
+		yelpSnippet: "",
+		yelpImageSrc: "",
+		yelpLink: "",
+		twitterWidget: '<a class="twitter-timeline" href="https://twitter.com/HECHOENMEXIC0" data-widget-id="605115848739549185">Tweets by @HECHOENMEXIC0</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?"http":"https";if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>',
+		instagramWidget: 'no Instagram account'
+	},
+	{
+		name: "ACC Pinnacle",
+		mapSrc: "Austin+Community+College:+Pinnacle+Campus",
+		latitude: 30.23152,
+		longitude: -97.883524,
+		content: "hi5",
+		wikipediaContent: 'wikpediaContent',
+		website: "http://www.austincc.edu/locations/campuses/pinnacle-campus",
+		yelpID: "austin-community-college-pinnacle-campus-austin-2?osq=acc+pinnacle",
+		yelpSnippet: "",
+		yelpImageSrc: "",
+		yelpLink: "",
+		twitterWidget: '<a class="twitter-timeline" href="https://twitter.com/accdistrict" data-widget-id="605116740406636544">Tweets by @accdistrict</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?"http":"https";if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>',
+		instagramWidget: 'no Instagram account'
+	},
+	{
+		name: "The Donut Hole",
+		mapSrc: "Austin+Community+College:+Pinnacle+Campus",
+		latitude: 30.23152,
+		longitude: -97.883524,
+		content: "hi5",
+		wikipediaContent: 'wikpediaContent',
+		website: "http://www.donutholeaustin.com/",
+		yelpID: "the-donut-hole-austin-2",
+		yelpSnippet: "",
+		yelpImageSrc: "",
+		yelpLink: "",
+		twitterWidget: 'no Twitter account found',
+		instagramWidget: 'no Instagram account'
+	},
+];
+
+///// Knockout Model
+var Place = function(data) {
+	this.name = ko.observable(data.name);
+	this.mapSrc = ko.observable(data.mapSrc);
+	this.latitude = ko.observable(data.latitude);
+	this.longitude = ko.observable(data.longitude);
+	this.content = ko.observable(data.content);
+	this.wikipediaContent = ko.observable(data.wikipediaContent);
+	this.website = ko.observable(data.website);
+	this.yelpID = ko.observable(data.yelpID);
+	this.yelpSnippet = ko.observable(data.yelpSnippet);
+	this.yelpImageSrc = ko.observable(data.yelpImageSrc);
+	this.yelpLink = ko.observable(data.yelpLink);
+	this.twitterWidget = ko.observable(data.twitterWidget);
+	this.instagramWidget = ko.observable(data.instagramWidget);
+}
+
+///// Knockout View Model
+var ViewModel = function() {
+  var self = this;
+
+  this.placeList = ko.observableArray([]);
+  places.forEach(function(placeItem) {
+    self.placeList.push( new Place(placeItem) );
+  });
+  this.currentPlace = ko.observable( this.placeList()[0] );
+
+  // YELP RELATED CODE
+  this.callYelpAPI = function() {
+		/////////////// YELP API STUFF
+		var YELP_CONSUMER_KEY = "N3DofJ4mIItoveG38brAHg";
+		var YELP_CONSUMER_SECRET = "tEeEHCEXinsVqj8L0vhJRT3kEBw";
+		var YELP_TOKEN = "tXgSSy2Kh_8J2gqA1dlKxosMs5vUAy_d";
+		var YELP_TOKEN_SECRET = "R1TICiVW89ybGSpEqy10JxiOTQ8";
+		var YELP_BASE_URL = "http://api.yelp.com/v2/"
+		var yelp_url = YELP_BASE_URL + 'business/' + self.currentPlace().yelpID();
+
+		//// OAUTH related code
+		// Generates a random number and returns it as a string for OAuthentication
+		function generate_nonce() {
+		  return (Math.floor(Math.random() * 1e12).toString());
+		}
+
+  	var parameters = {
+      oauth_consumer_key: YELP_CONSUMER_KEY,
+      oauth_token: YELP_TOKEN,
+      oauth_nonce: generate_nonce(),
+      oauth_timestamp: Math.floor(Date.now()/1000),
+      oauth_signature_method: 'HMAC-SHA1',
+      oauth_version : '1.0',
+      callback: 'cb'  // This is crucial to include for jsonp implementation in
+                      // AJAX or else the oauth-signature will be wrong.
+  	};
+
+  	// USE BELOW FROM UDACITY FORUM EXAMPLE TO SIGN OAUTH TO USE YELP API and twitter api and google plus
+  	var encodedSignature = oauthSignature.generate('GET',yelp_url, parameters, YELP_CONSUMER_SECRET, YELP_TOKEN_SECRET);
+  	parameters.oauth_signature = encodedSignature;
+  	//// end OAuth related code
+
+  	var settings = {
+      url: yelp_url,
+      data: parameters,
+      cache: true,  // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
+      dataType: 'jsonp',
+      success: function(results) {
+        self.updateYelpContent(results);
+      },
+      fail: function() {
+        console.log('fail');
+      }
+    };
+    $.ajax(settings);
+  } /////// END callYelpAPI function
+
+  self.callYelpAPI();
+
+  this.updateYelpContent = function(results) {
+  	self.currentPlace().yelpImageSrc(results.image_url);
+  	self.currentPlace().yelpSnippet(results.snippet_text.toString());
+  	self.currentPlace().yelpLink(results.url);
+  	var yelpImage = document.getElementsByClassName("yelpImage")[0];
+  	if ($(".yelpSnippet").length > 0) {
+  		$(".yelpSnippet").remove();
+  	};
+  	var br = document.createElement("br")
+  	var newParagraph = document.createElement("p");
+  	var snippet = document.createTextNode(results.snippet_text.toString())
+  	newParagraph.appendChild(snippet);
+  	$(newParagraph).addClass("yelpSnippet");
+  	$(br).insertAfter(yelpImage);
+  	$(function() {
+  	  $('span.stars').stars(results.rating);
+  	});
+  } // end updateYelpContent function
+  ////// end yelp stuff
+
+  // GOOLE MAPS STUFF FOR CHANGEPLACE1
+  myLatlng = new google.maps.LatLng(30.2433481,-97.8703633);
+  mapOptions = {
+    zoom: 13,
+    center: myLatlng
+  }
+  var sv = new google.maps.StreetViewService();
+  markers = [];
+
+  // function called when you click on a place in the list
+  this.changePlace1 = function(place, event) {
+ 	  self.currentPlace(place);
+ 	  self.callYelpAPI();
+		// all placeList not bold
+		var placeItems = document.getElementsByClassName("placeListItem");
+		for (var i = 0; i < placeItems.length; i++) {
+			var placeItem = placeItems[i];
+			placeItem.style.fontWeight = "normal";
+		};
+
+		var context = ko.contextFor(event.target);
+		var tabsDivArray = document.getElementsByClassName("tabsDiv");
+		for (var i = 0; i < tabsDivArray.length; i++) {
+			var tabsDiv = tabsDivArray[i];
+			if ($(tabsDiv).is(':visible')) {
+				$(tabsDiv).slideToggle();
+			};
+		};
+
+		var tabs = document.getElementById("tabsDiv" + context.$index());
+		// also want to close all open tabDivs...
+		$(tabs).slideToggle();
+		$(".extra").remove()
+		tabs.style.display = "inline-block";
+		$(tabs).prev()[0].style.fontWeight = 'bold'
+		// insert blank div behind to lower other list items
+		$(event.target).next().append("<div class='extra' stye='position: relative;height: 200px;'></div>");
+
+		var nextID = $(event.target).next()[0].id
+		var targetIndex = nextID.substring(nextID.length - 1, nextID.length);
+		document.getElementById("tabRadioWiki" + targetIndex).checked = true;
+
+		// GOOGLE MAPS STUFF called on ChangePlace
+  	// panTo place
+  	var position = new google.maps.LatLng(place.latitude(),place.longitude());
+
+  	window.map.panTo(position);
+  	window.map.setZoom(17);
+
+  	markers.forEach(function(marker) {
+  		if (marker.position.A.toFixed(6) == place.latitude && marker.position.F.toFixed(6) == place.longitude) {
+  			marker.setAnimation(google.maps.Animation.BOUNCE);
+  			window.setTimeout(function() {
+  				marker.setAnimation(null);
+  			}, 3500);
+  		};
+  	})
+  }; // end function changePlace1
+
+  ///// START GOOGLE MAPS STUFF
+  var image = {url: 'customPin.svg'};
+  var shape = {
+		coords: [1, 1, 1, 30, 46, 30, 46, 1],
+		type: 'poly'
+   };
+
+   // START initializeMap function
+  this.initializeMap = function() {
+  	var content = document.createElement("DIV");
+  	var streetview = document.createElement("DIV");
+  	streetview.style.width = "200px";
+  	streetview.style.height = "200px";
+  	content.appendChild(streetview);
+
+  	searchMarkers = [];
+    window.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var defaultBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(30.2, -97.92),
+        new google.maps.LatLng(30.3, -97.85));
+    window.map.fitBounds(defaultBounds);
+
+    places.forEach(function(place) {
+    	var title = document.createElement("DIV");
+    	title.innerText = places[places.indexOf(place)].name;
+    	content.appendChild(title);
+    	var myLatlng = new google.maps.LatLng(place.latitude, place.longitude);
+    	var marker = new google.maps.Marker({
+    	    position: myLatlng,
+    	    map: map,
+    	    icon: 'customPinSmall.png',
+    	    shape: shape,
+    	    animation: google.maps.Animation.DROP
+    	});
+
+    	var infowindow = new google.maps.InfoWindow({
+    	  size: new google.maps.Size(150,50),
+    	  content: '<div id="content">'+ '<h3>' + place.name + '</h3>'+ '</div>'
+    	});
+    	markers.push(marker);
+    	google.maps.event.addListener(marker, 'click', function() {
+    	  map.setZoom(15);
+    	  map.panTo(marker.getPosition());
+    	  marker.setAnimation(google.maps.Animation.BOUNCE);
+    	  window.setTimeout(function() {
+    	  	marker.setAnimation(null);
+    	  }, 3300);
+
+    	  infowindow.open(map, marker);
+    	  clickedMarker = marker;
+				var pin = new google.maps.MVCObject();
+				function openInfoWindow(marker) {
+				   pin.set("position", marker.getPosition());
+				   infowindow.open(map, marker);
+				}
+    	});// end addListener marker from line ~335
+
+    }); // end places.forEach form  316
+
+  	// Create the search box and link it to the UI element.
+  	var input = (document.getElementById('mySearchBox'));
+  	var searchBox = new google.maps.places.SearchBox((input));
+
+	  // Listen for the event fired when the user selects an item from the
+	  // search list. Retrieve the matching places for that item.
+	  google.maps.event.addListener(searchBox, 'places_changed', function() {
+	    var searchPlaces = searchBox.getPlaces();
+
+	    if (searchPlaces.length == 0) {
+	      return;
+	    }
+	    for (var i = 0, searchMarker; searchMarker = searchMarkers[i]; i++) {
+	      searchMarker.setMap(null);
+	    }
+
+	    // For each place, get the icon, place name, and location.
+	    searchMarkers = [];
+	    var searchBounds = new google.maps.LatLngBounds();
+	    for (var i = 0, place; place = searchPlaces[i]; i++) {
+	      var image = {
+	        url: place.icon,
+	        size: new google.maps.Size(71, 71),
+	        origin: new google.maps.Point(0, 0),
+	        anchor: new google.maps.Point(17, 34),
+	        scaledSize: new google.maps.Size(25, 25)
+	      };
+
+	      // Create a marker for each place.
+	      var searchMarker = new google.maps.Marker({
+	        map: map,
+	        icon: image,
+	        title: place.name,
+	        position: place.geometry.location
+	      });
+
+	      searchMarkers.push(searchMarker);
+
+	      searchBounds.extend(place.geometry.location);
+	    }
+	    map.fitBounds(searchBounds);
+	  });
+	  // Bias the SearchBox results towards places that are within the bounds of the
+	  // current map's viewport.
+	  google.maps.event.addListener(map, 'bounds_changed', function() {
+	    var bounds = map.getBounds();
+	    searchBox.setBounds(bounds);
+	  });
+  } // END this.initializeMap
+
+  google.maps.event.addDomListener(window, 'load', this.initializeMap);
+}////**** END
+
+//For showing the Yelp star rating
+$.fn.stars = function(rating) {
+  return $(this).each(function() {
+    $(this).html($('<span />').width(Math.max(0, (Math.min(5, parseFloat(rating)))) * 16));
+  });
+}
+
+ko.applyBindings(new ViewModel());
