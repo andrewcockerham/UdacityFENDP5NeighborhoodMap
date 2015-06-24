ad3a060dc83ce6560466d443acf50bb2a3dbdb8c
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-06-23 21:13:09 -0500

    log place

--- script.js
+++ script.js
@@ -199,7 +199,7 @@ var ViewModel = function() {
 	  this.changePlace1 = function(place, event) {
 	  	console.log("in changeplace")
 	  	console.log("place ");
-	  	console.log(place.name);
+	  	console.log(place);
 	 	  self.currentPlace(place);
 	 	  // self.callYelpAPI();
 	 	  // infowindow.open(map);
