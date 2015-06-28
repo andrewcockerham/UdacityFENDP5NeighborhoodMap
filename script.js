b61c47a86c9eceae23b793338725e6ef1223e407
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-06-28 14:40:21 -0500

    log place

--- script.js
+++ script.js
@@ -203,8 +203,9 @@ var ViewModel = function() {
 	    // infowindow.setContent('<div id="content">' + '<h3 id="placeName">' + place.name + '</h3>'+ '</div>')
 
 	    self.closeDropdown();
-	    self.showDropdown(self.currentPlace());
-	    // self.showDropdown(place);
+	    // self.showDropdown(self.currentPlace());
+	    self.showDropdown(place);
+	    console.log(place);
 
 			// all placeList not bold
 			// var placeItems = document.getElementsByClassName("placeListItem");
