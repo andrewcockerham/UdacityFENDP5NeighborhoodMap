205480b40936d755670faf5f5a249df15937bb91
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-06-27 21:29:29 -0500

    add col-md-5 to .extra

--- script.js
+++ script.js
@@ -230,7 +230,7 @@ var ViewModel = function() {
 			// tabs.style.display = "inline-block";
 			$(tabs).prev()[0].style.fontWeight = 'bold';
 			// insert blank div behind to lower other list items
-			$(event.target).next().append("<div class='extra' stye='position: relative;height: 200px;'></div>");
+			$(event.target).next().append("<div class='extra col-md-5 col-sm-12' stye='position: relative;height: 200px;'></div>");
 
 			var nextID = $(event.target).next()[0].id;
 			var targetIndex = nextID.substring(nextID.length - 1, nextID.length);
