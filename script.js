5115281c3e5720a2444e89942c51b96e95cfd97c
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-07-26 19:52:50 -0500

    log place.name()

--- script.js
+++ script.js
@@ -585,7 +585,7 @@ var ViewModel = function() {
   this.showInfoBox = function(infobox, place, map, marker) {
   	console.log("in showInfoBox")
   	console.log(place)
-  	console.log(place())
+  	console.log(place.name())
   	infobox.open(map, marker);
   	infobox.setContent('<div id="infobox">' + '<h2 id="placeName"><a href="' + place.website + '">' + place.name() + '</a>' + '</h3><h4>' + place.address + '</h4></div>');
   }
