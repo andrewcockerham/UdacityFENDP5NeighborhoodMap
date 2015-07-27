1a87df92eabfe76598fd6df8bf00c57e03a9e364
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-07-26 19:25:05 -0500

    add parent

--- script.js
+++ script.js
@@ -585,7 +585,7 @@ var ViewModel = function() {
   this.showInfoBox = function(infobox, place, map, marker) {
   	console.log("in showInfoBox")
   	infobox.open(map, marker);
-  	infobox.setContent('<div id="infobox">' + '<h2 id="placeName"><a href="' + place.website + '">' + place.name + '</a>' + '</h3><h4>' + place.address + '</h4></div>');
+  	infobox.setContent('<div id="infobox">' + '<h2 id="placeName"><a href="' + place.website + '">' + place.name() + '</a>' + '</h3><h4>' + place.address + '</h4></div>');
   }
 
   // YELP RELATED CODE
