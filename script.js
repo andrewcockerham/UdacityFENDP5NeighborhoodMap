f9f55a2d36a29e365c1ff5ebbf8f5616536a37f4
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-08-02 16:08:46 -0500

    clean up

--- script.js
+++ script.js
@@ -479,27 +479,26 @@ var ViewModel = function() {
   	newParagraph.appendChild(snippet);
   	$(newParagraph).addClass('yelpSnippet');
   	$(br).insertAfter(yelpImage);
-  	console.log(results.rating);
   	$(function() {
-  	  $("span.stars").stars(results.rating);
-  	  // $('span.stars').stars(results.rating);
+  	  // $("span.stars").stars(results.rating);
+  	  $('span.stars').stars(results.rating);
   	});
   }; // end updateYelpContent function
   ////// end yelp stuff
 
 	//For showing the Yelp star rating
-	// $.fn.stars = function(rating) {
-	//   return $(this).each(function() {
-	//     $(this).html($('<span />').width(Math.max(0, (Math.min(5, parseFloat(rating)))) * 16));
-	//   });
-	// };
+	$.fn.stars = function(rating) {
+	  return $(this).each(function() {
+	    $(this).html($('<span />').width(Math.max(0, (Math.min(5, parseFloat(rating)))) * 16));
+	  });
+	};
 
   google.maps.event.addDomListener(window, 'load', this.initializeMap);
 }; ////**** END VIEW MODEL
 
-$.fn.stars = function(rating) {
-  return $(this).each(function() {
-    $(this).html($('<span />').width(Math.max(0, (Math.min(5, parseFloat(rating)))) * 16));
-  });
-};
+// $.fn.stars = function(rating) {
+//   return $(this).each(function() {
+//     $(this).html($('<span />').width(Math.max(0, (Math.min(5, parseFloat(rating)))) * 16));
+//   });
+// };
 ko.applyBindings(new ViewModel());
