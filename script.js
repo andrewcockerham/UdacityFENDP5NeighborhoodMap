f135802f39e9f7588003bb820a68533e01feef49
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-08-02 12:05:03 -0500

    comment logs

--- script.js
+++ script.js
@@ -407,7 +407,7 @@ var ViewModel = function() {
   	*		marker  - the marker that was clicked
   */
   this.showInfoBox = function(infobox, place, map, marker) {
-  	console.log(place.name())
+  	// console.log(place.name())
   	console.log(self.currentPlace().name())
   	infobox.open(map, marker);
   	infobox.setContent('<div id="infobox">' + '<h2 id="placeName"><a href="' + place.website.toString() + '">' + self.currentPlace().name() + '</a>' + '</h3><h4>' + place.address() + '</h4></div>');
