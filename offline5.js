03a9c8c332443b861d0dd99e481c6eac591529f9
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-08-24 15:14:40 +0800

    offline5.js

--- offline5.js
+++ offline5.js
@@ -93,7 +93,8 @@
 		var b;
 		return b = document.createElement("img"), b.onerror = a.markDown, b.onload = a.markUp, void(b.src = a.getOption("checks.image.url"))
 	}, a.checks.down = a.markDown, a.checks.up = a.markUp, a.check = function() {
-		return a.trigger("checking"), a.checks[a.getOption("checks.active")]()
+		return a.checks[a.getOption("checks.active")]()
+		// a.trigger("checking"),
 	}, a.confirmUp = a.confirmDown = a.check, a.onXHR = function(a) {
 		var b, c, e;
 		return e = function(b, c) {
