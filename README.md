4b19e482083d7301f7485d241e012e309ca646e9
Andrew Cockerham <cockerham.andrew@gmail.com>
2015-08-29 21:52:00 -0500

    fix other udacity review things

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 This is Project 5 for the [Udacity Front End Web Developer Nanodegree](https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001).
 
-Load a full demo via github pages [here](http://andrewcockerham.github.io/UdacityFENDP5NeighborhoodMap/) or open `index.html`.
+Load a full demo via github pages [here](http://andrewcockerham.github.io/UdacityFENDP5NeighborhoodMap/).
 
 Clicking on items in the list on the left will open a dropdown with tabs for Twitter, Yelp, the place website, and Instagram.  The place marker will also show its name with a link to its website and its address.
 
--- gulpfile.js
+++ gulpfile.js
@@ -5,27 +5,27 @@ var jshint = require('gulp-jshint');
 var htmlhint = require("gulp-htmlhint");
 
 gulp.task('jscs', function() {
-  gulp.src('script.js')
-  .pipe(jscs())
-  .pipe(notify({
-    title: 'JSCS',
-      message: 'JSCS Passed. Let it fly!'
-  }));
+	gulp.src('script.js')
+	.pipe(jscs())
+	.pipe(notify({
+		title: 'JSCS',
+			message: 'JSCS Passed. Let it fly!'
+	}));
 });
 
 gulp.task('lint', function() {
-  gulp.src('*.js')
-  .pipe(jshint('.jshintrc'))
-  .pipe(jshint.reporter('jshint-stylish'))
-  .pipe(jshint.reporter('fail'))
-  .pipe(notify({
-      title: 'JSHint',
-      message: 'JSHint Passed. Let it fly!',
-  }));
+	gulp.src('*.js')
+	.pipe(jshint('.jshintrc'))
+	.pipe(jshint.reporter('jshint-stylish'))
+	.pipe(jshint.reporter('fail'))
+	.pipe(notify({
+			title: 'JSHint',
+			message: 'JSHint Passed. Let it fly!',
+	}));
 });
 
 gulp.task('html', function () {
-  gulp.src("index.html")
-    .pipe(htmlhint())
-    .pipe(htmlhint.reporter());
+	gulp.src("index.html")
+		.pipe(htmlhint())
+		.pipe(htmlhint.reporter());
 });
